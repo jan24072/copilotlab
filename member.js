b7baf591205bbf56d292c2c0067ef4a4e3ce2555
jan24072 <janakisaisaran24@gmail.com>
2024-12-26 10:55:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+
+    var skills = {
+        name: "John",
+        age: 25,
+        job: "Developer",
+        skills: ["HTML", "CSS", "JS", "PHP"]
+    };
+
+    return skills;
+}
